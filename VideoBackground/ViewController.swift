96d67b4e2175ca54202d022ab782436fe25324e3
adanigr <adanigr@gmail.com>
2020-07-10 14:14:14 -0500

    Initial Commit

---
+++ VideoBackground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoBackground
+//
+//  Created by Adan Reséndiz on 10/07/20.
+//  Copyright © 2020 Adan Reséndiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
